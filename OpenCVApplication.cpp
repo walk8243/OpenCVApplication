9c2f3d2841474fca66e891d0167494e454608639
walk8243 <walk8243@gmail.com>
2021-01-05 22:56:03 +0900

    比較結果を表示

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -1,7 +1,4 @@
-﻿// OpenCVApplication.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
-
-#include <opencv2/core.hpp>
+﻿#include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
@@ -48,8 +45,6 @@ int main()
     findContours(matDilate, vctContours, RETR_LIST, CHAIN_APPROX_NONE);
 
     int i = 0;
-
-    // 青色で輪郭を描画.
     for (auto contour = vctContours.begin(); contour != vctContours.end(); contour++) {
         cv::drawContours(targetImage, vctContours, i, CV_RGB(0, 0, 255), 2);
         i++;
@@ -61,14 +56,3 @@ int main()
     destroyAllWindows();
     return 0;
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
